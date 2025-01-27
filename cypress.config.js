99d92a70709508d67db82e2d7dd45a5b25445d26
Matheus Moreira De Souza <matheus.moreira@ifood.com.br>
2025-01-26 22:34:19 -0300

    update projectID cypress_cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "q56pho",
+  projectId: "5h3b2d",
   e2e: {
     specPattern: "cypress/tests/**.cy.js",
     setupNodeEvents(on, config) {
